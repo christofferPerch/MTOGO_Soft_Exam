54e7a92bab662fb2a0a882a164e45a5846b6b483
christofferPerch <cph-cn332@cphbusiness.dk>
2024-11-06 11:07:32 +0100

    Setup crud for restaurant Fixes #5 #6 #7 #8 #9

---
+++ MTOGO.Services.RestaurantAPI/Models/FoodCategory.cs
@@ -0,0 +1,39 @@
+﻿namespace MTOGO.Services.RestaurantAPI.Models
+{
+    public class FoodCategory
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public Category Category { get; set; }
+
+        public Restaurant Restaurant { get; set; }
+    }
+
+    public enum Category
+    {
+        Undefined = 0,
+        Chinese = 1,
+        Pizza = 2,
+        Burger = 3,
+        Italian = 4,
+        Indian = 5,
+        Mexican = 6,
+        Sushi = 7,
+        Thai = 8,
+        American = 9,
+        Mediterranean = 10,
+        Vegetarian = 11,
+        Vegan = 12,
+        Seafood = 13,
+        French = 14,
+        Dessert = 15,
+        BBQ = 16,
+        Steakhouse = 17,
+        MiddleEastern = 18,
+        FastFood = 19,
+        Healthy = 20,
+        Bakery = 21,
+        Breakfast = 22,
+        Coffee = 23
+    }
+}
