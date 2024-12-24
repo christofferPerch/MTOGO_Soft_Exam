618ad3dd9f5bebbbc6e2a0dec6f0b286941922c4
christofferPerch <cph-cn332@cphbusiness.dk>
2024-12-24 12:40:36 +0100

    prometheus included in docker

--- MTOGO.Monitoring/Program.cs
+++ MTOGO.Monitoring/Program.cs
@@ -1,6 +1,15 @@
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+app.UseHttpMetrics(); 
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapMetrics(); 
+});
 
 app.Run();
